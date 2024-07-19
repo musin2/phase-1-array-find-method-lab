74740186bb53ccf0e53d53da8a1a875013fcc74a
musin2 <muhsindaud75@gmail.com>
2024-07-19 21:11:58 +0300

    adds lab code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+ 
+// function superbowlWin(record) {
+//     let res = record.find( record => record.result == "W");        
+//         console.log(res);
+//         if (res !== "") {
+//             return undefined;
+//         } else{ return res.year}
+// }
+
+
+superbowlWin = function (record) {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
